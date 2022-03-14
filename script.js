32687cce2a9e4551f86f47ecb2a53a1b676ac3cc
Suleman Ahmad <87057938+Sulman23@users.noreply.github.com>
2022-03-14 09:48:50 +0500

    finalized

--- script.js
+++ script.js
@@ -1,15 +1,16 @@
 // selecting elements
- let rupees = document.getElementById("rupees");
- let dollar = document.getElementById("dollar");
+    let rupees = document.getElementById("rupees");
+    let dollar = document.getElementById("dollar");
 
- console.log(rupees);
 
-// onChange event listner
+// onChange Rs to dollar convert
+    rupees.onchange = () => {
+        let value = rupees.value * 180.80;
+        dollar.value = value.toFixed(2);
+    }
 
-rupees.onchange = () => {
-    let value = rupees.value;
-console.log(value);
-
-}
-
-alert("working");
+// onChange Dollar to Rupess convert
+    dollar.onchange = () => {
+        let value = dollar.value / 0.0055;
+        rupees.value = value.toFixed(2);
+    }
